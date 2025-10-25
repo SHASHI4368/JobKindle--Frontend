e19af778023cae97003b78ae2fb1cf4b3629a3f4
Gurunayake GMRSB <139693794+SHASHI4368@users.noreply.github.com>
2025-10-25 14:19:15 +0530

    Revert "Feature/14 develop connect backend to create job post section"

--- src/app/api/config/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-  });
-}
--- src/types/auth.ts
+++
@@ -1,4 +0,0 @@
-export type Auth = {
-  isLogin: boolean;
-  dialogOpen: boolean;
-};
